460534215ea7ebc55e30b3d63c608811f260ee49
doprz <52579214+doprz@users.noreply.github.com>
2024-12-02 20:04:59 -0600

    chore: disable auto-imports

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     srcDir: "src",
     extensionApi: "chrome",
     modules: ["@wxt-dev/module-solid"],
+    imports: false, // Disable auto-imports
     manifest: {
         permissions: ["webRequest", "tabs"],
         host_permissions: ["*://*.la.utexas.edu/*"],
